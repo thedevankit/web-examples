b1815d819ef54cf3d64e3c6630e76991c79a810f
Ankitkumar Singh <ankit-k-singh@users.noreply.github.com>
2018-04-17 22:50:03 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo "hello everyone its ajaxtutorials by ankit" ;
+
+
+
+ ?>
---
+++ database.php
@@ -0,0 +1,14 @@
+<?php 
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+echo $name;
+
+echo $email;
+ 
+echo $message;
+
+
+ ?>
